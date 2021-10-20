f365dc53de1f2ace6248698a3775f6d2c3397f0b
daimafengzi <65541893+daimafengzi@users.noreply.github.com>
2021-10-21 00:56:36 +0800

    Add files via upload

--- qdmd.js
+++ qdmd.js
@@ -13,8 +13,6 @@ const moment = require('moment');
 const notify = $.isNode() ? require('./sendNotify') : '';
 //Node.js用户请在jdCookie.js处填写京东ck;
 const jdCookieNode = $.isNode() ? require('./jdCookie.js') : '';
-let jdNotify = false;//是否关闭通知，false打开通知推送，true关闭通知推送
-const randomCount = $.isNode() ? 1 : 1;//运行2次。相隔1秒一次
 //IOS等用户直接用NobyDa的jd cookie
 let cookiesArr = [], cookie = '', message;
 if ($.isNode()) {
@@ -33,8 +31,7 @@ let wait = ms => new Promise(resolve => setTimeout(resolve, ms));
     $.msg($.name, '【提示】请先获取京东账号一cookie\n直接使用NobyDa的京东签到获取', 'https://bean.m.jd.com/bean/signIndex.action', { "open-url": "https://bean.m.jd.com/bean/signIndex.action" });
     return;
   }
-  //await wait(50000)延时50000毫秒
-  for (let j = 0; j < randomCount; ++j)
+
     for (let i = 0; i < cookiesArr.length; i++) {
       if (cookiesArr[i]) {
         cookie = cookiesArr[i];
