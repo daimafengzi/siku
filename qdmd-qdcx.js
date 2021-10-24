0a09dda522c91e968aa1db572a1e2acd89288e7e
daimafengzi <65541893+daimafengzi@users.noreply.github.com>
2021-10-25 00:19:38 +0800

    Add files via upload

--- qdmd-qdcx.js
+++ qdmd-qdcx.js
@@ -71,7 +71,7 @@ async function price() {
 async function jstoken() {
 	return new Promise(async resolve => {
 		const options = {
-			"url": `https://api.m.jd.com/?functionId=signFreeHome&body={%22linkId%22:%22PiuLvM8vamONsWzC0wqBGQ%22}&t=1634366231525&appid=activities_platform&client=H5&clientVersion=1.0.0&h5st=20211016143711647%3B6757289765640134%3B9cca1%3Btk02w78391b3018nByl3hGPr3zJ9B2F5PFAnWb8e%2BIfxQMYHnNuDG%2FWhFWH4xcExN7Nt2p1u7Vg4fTapGXcFn3iRDvV9%3B0dce837abd9e94a960d8fdcb4d82d9958f6692dc9892c8f75273ab53f0ab2729%3B3.0%3B1634366231647`,
+			"url": `https://api.m.jd.com/?functionId=signFreeSignList&body={%22linkId%22:%22PiuLvM8vamONsWzC0wqBGQ%22,%22pageNum%22:1,%22pageSize%22:10}&t=1635091933458&appid=activities_platform&client=H5&clientVersion=1.0.0`,
 			"headers": {
 				"Accept": "application/json,text/plain, */*",
 				"Content-Type": "application/x-www-form-urlencoded",
@@ -79,7 +79,7 @@ async function jstoken() {
 				"Accept-Language": "zh-cn",
 				"Connection": "keep-alive",
 				"Cookie": cookie,
-				"Referer": "https://signfree.jd.com/?activityId=PiuLvM8vamONsWzC0wqBGQ&lng=117.020205&lat=25.074926&sid=3347d4988324d948d7e998cb7b7cdbbw&un_area=16_1362_44319_55501",
+				"Referer": "https://signfree.jd.com/order?activityId=PiuLvM8vamONsWzC0wqBGQ",
 				"User-Agent": $.isNode() ? (process.env.JD_USER_AGENT ? process.env.JD_USER_AGENT : (require('./USER_AGENTS').USER_AGENT)) : ($.getdata('JDUA') ? $.getdata('JDUA') : "jdapp;iPhone;9.4.4;14.3;network/4g;Mozilla/5.0 (iPhone; CPU iPhone OS 14_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148;supportJDSHWK/1")
 			}
 		}
