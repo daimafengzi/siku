6d60bea6e6b7137a84652744f4cebd4f3b2ff67e
daimafengzi <65541893+daimafengzi@users.noreply.github.com>
2022-01-17 00:58:30 +0800

    Add files via upload

--- dm_199-198.js
+++ dm_199-198.js
@@ -76,12 +76,12 @@ function exchange() {
           console.log(data);
           if (safeGet(data)) {
             data = JSON.parse(data);
-            console.log(`【9.9特卖9减3优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}\n`);
+            console.log(`【伊利199-198元优惠券抢券结果】${JSON.stringify(data)}\n`);
 			//自行添加通知项目开始
-			var str="{"+data.subCodeMsg+"}";
-			if(str.indexOf('领取成功') !=-1){
-				//notify.sendNotify($.name, `京东账号  ${$.nickName || $.UserName}\n【伊利199-198元优惠券抢券结果】${JSON.stringify(data.subCodeMsg)}`);
-				}
+			//var str="{"+data.subCodeMsg+"}";
+			//if(str.indexOf('领取成功') !=-1){
+				notify.sendNotify($.name, `京东账号  ${$.nickName || $.UserName}\n【伊利199-198元优惠券抢券结果】${JSON.stringify(data)}`);
+				//}
 			//自行添加通知项目结束
           }
         }
